2b371be0f8697ac1a919fe75fe71f864a1b96878
Sahib J. Leo <sahib.alejandro@gmail.com>
2012-07-01 00:11:02 -0500

    Agregado el GitHUB Ribbon :)

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -7,5 +7,7 @@
   $this->prependJsFiles('lib/jquery.min.js','lib/bootstrap.js', 'main-script.js')
     ->includeJsFiles(false);
   ?>
+  <!-- GitHub ribbon yay! -->
+  <a href="https://github.com/sahibalejandro/control-de-gastos/"><img style="position: absolute; top: 0; left: 0; border: 0; z-index: 1031;" src="https://s3.amazonaws.com/github/ribbons/forkme_left_orange_ff7600.png" alt="Fork me on GitHub"></a>
   </body>
 </html>
