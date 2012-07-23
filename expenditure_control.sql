5f17a491dde5be9982166432159c31f876e0a825
Sahib J. Leo <sahib.alejandro@gmail.com>
2012-07-23 12:01:14 -0500

    New config and database schema

---
+++ expenditure_control.sql
@@ -0,0 +1,63 @@
+-- Adminer 3.3.4 MySQL dump
+
+SET NAMES utf8;
+SET foreign_key_checks = 0;
+SET time_zone = 'SYSTEM';
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+CREATE DATABASE `expenditure_control` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `expenditure_control`;
+
+DROP TABLE IF EXISTS `accounts`;
+CREATE TABLE `accounts` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `users_id` int(10) unsigned NOT NULL,
+  `name` varchar(50) NOT NULL,
+  `creation_date` date NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `accounts_ibfk_2` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `movements`;
+CREATE TABLE `movements` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `users_id` int(10) unsigned NOT NULL,
+  `accounts_id` int(11) NOT NULL,
+  `amount` decimal(9,2) NOT NULL,
+  `date` date NOT NULL,
+  `type` tinyint(1) NOT NULL,
+  `concept` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `cuenta_id` (`accounts_id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `movements_ibfk_6` FOREIGN KEY (`accounts_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `movements_ibfk_5` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `payments`;
+CREATE TABLE `payments` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `users_id` int(10) unsigned NOT NULL,
+  `amount` decimal(9,2) unsigned NOT NULL,
+  `concept` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `users_id` (`users_id`),
+  CONSTRAINT `payments_ibfk_2` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(50) NOT NULL,
+  `pass` char(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `creation_date` datetime NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+-- 2012-07-23 11:59:55
