47c06644e726dd27b972ea3ff91af08a95eeaad7
Sahib J. Leo <sahib.alejandro@gmail.com>
2012-08-25 17:45:55 -0500

    Database updated to use the twitter signin implementation

--- expenditure_control.sql
+++ expenditure_control.sql
@@ -1,12 +1,9 @@
--- Adminer 3.3.4 MySQL dump
+-- Adminer 3.5.1 MySQL dump
 
 SET NAMES utf8;
 SET foreign_key_checks = 0;
 SET time_zone = 'SYSTEM';
 SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
-
-CREATE DATABASE `expenditure_control` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `expenditure_control`;
 
 DROP TABLE IF EXISTS `accounts`;
 CREATE TABLE `accounts` (
@@ -31,8 +28,9 @@ CREATE TABLE `movements` (
   `type` tinyint(1) NOT NULL,
   `concept` varchar(50) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `users_id` (`users_id`),
   KEY `accounts_id` (`accounts_id`),
+  KEY `users_id` (`users_id`),
+  KEY `type` (`type`),
   CONSTRAINT `movements_ibfk_5` FOREIGN KEY (`users_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `movements_ibfk_6` FOREIGN KEY (`accounts_id`) REFERENCES `accounts` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
@@ -53,12 +51,10 @@ CREATE TABLE `payments` (
 DROP TABLE IF EXISTS `users`;
 CREATE TABLE `users` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `email` varchar(50) NOT NULL,
-  `passwd` char(32) CHARACTER SET ascii COLLATE ascii_bin NOT NULL,
-  `name` varchar(100) DEFAULT NULL,
+  `screen_name` varchar(50) NOT NULL,
   `creation_date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
--- 2012-07-28 02:10:32
+-- 2012-08-25 17:45:09
