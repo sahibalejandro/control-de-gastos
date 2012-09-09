10550ed365fa6e9c24dce1b7c2a59d046554dcb1
Sahib J. Leo <sahib.alejandro@gmail.com>
2012-09-09 17:46:15 -0500

    Views for recover password is not needed anymore

--- application/views/home/recover-password-email.php
+++
@@ -1,5 +0,0 @@
-<p>Hola <?php echo $this->QuarkStr->esc($name); ?>!</p>
-<p>Aqu&iacute; est&aacute; tu nueva contrase&ntilde;a de acceso:</p>
-<p>Contrase&ntilde;a: <?php echo $this->QuarkStr->esc($passwd); ?><p>
-<p>Cuando inicies sesi&oacute;n podr&aacute;s cambiar esta contrase&ntilde;a por una m&aacute;s f&aacute;cil de recordar.</p>
-<p>Guarda esta informaci&oacute;n y no la compartas con tus amigos.</p>
