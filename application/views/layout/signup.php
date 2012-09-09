b5085ea1b2ff2b66fb5c56b9c3011197ff49ef70
Sahib J. Leo <sahib.alejandro@gmail.com>
2012-09-09 18:49:52 -0500

    Copy fixes and remove signup-email.php view

--- application/views/layout/signup.php
+++ application/views/layout/signup.php
@@ -9,7 +9,7 @@ Falló la comunicación con twitter, intenta más tarde. (<?php echo $this->Quar
 <?php
 else:
 ?>
-Conectate usando tu cuenta de Twitter, no no necesitas registrarte y es más seguro.
+Conectate usando tu cuenta de Twitter, no necesitas registrarte y es más seguro.
 <a href="<?php echo $auth_url ?>" id="btn_signin">Inicia sesión con Twitter</a>
 <?php
 endif;
