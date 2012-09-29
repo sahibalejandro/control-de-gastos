b535e76ba4a72093f232ccd7131dc3bc73b8331c
Sahib J. Leo <sahib.alejandro@gmail.com>
2012-09-28 20:17:19 -0500

    Updated to re-relased QuarkPHP 3.5.2

--- application/views/layout/footer.php
+++ application/views/layout/footer.php
@@ -6,7 +6,7 @@
   <!-- BEGIN: Footer = = = = = = = = = = = = = = = = = = = = = = = = = = = = = -->
   <div id="footer">
     gassto&reg; 2012 <a href="http://sahibalejandro.com">Sahib Alejandro Jaramillo Leo</a><br />
-    Powered by <a href="http://quarkphp.com">QuarkPHP Framework</a>
+    Powered by <a href="http://quarkphp.com">QuarkPHP Framework <?php echo Quark::VERSION; ?></a>
   </div>
   <!-- END: Footer = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = -->
 </body>
